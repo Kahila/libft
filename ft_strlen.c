962636e5c00af02c3e0ba571c2962e3432cd382f
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2019-05-22 11:59:03 +0200

    akalombo

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 10:50:49 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/22 11:43:02 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/22 11:58:47 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int             ft_strlen(char *str)
 {
-    int intCounter = 0;
+    int intCounter;
+   	intCounter	= 0;
 
     while (str[intCounter] != 0)
         intCounter++;
