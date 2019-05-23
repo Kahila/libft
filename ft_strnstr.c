fb3e6f42b9d724579228399afc2411cb538fe1a9
Ado KALOMBO <akalombo@c4r2s2.wethinkcode.co.za>
2019-05-23 08:02:59 +0200

    akalombo

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,18 +6,18 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 05:50:15 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/23 06:36:05 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/23 08:02:03 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char        *ft_strnstr (const char *haystack, const char *needle, int len)
+char        *ft_strnstr (const char *haystack, const char *needle, size_t len)
 {
-
     const char *s1;
     const char *s2;
-    if (*needle == '\0')
+
+    if (*needle == '\0' || str == 0)
         return (char *)(haystack);
 
     while (*haystack != '\0')
@@ -39,16 +39,4 @@ char        *ft_strnstr (const char *haystack, const char *needle, int len)
         else break;
     }
     return (NULL);
-}
-
-
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    char name[30] = "adonis";
-    char find[10] = "o";
-
-    printf("%s\n", ft_strnstr(name, find, 1)); 
-    return (0);
 }
