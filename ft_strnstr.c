146f75e87cdb0a2b4b68f6d6a92ee55284821254
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2019-05-23 11:11:59 +0200

    akalombo

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 05:50:15 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/23 08:02:03 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/23 11:11:08 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char        *ft_strnstr (const char *haystack, const char *needle, size_t len)
     const char *s1;
     const char *s2;
 
-    if (*needle == '\0' || str == 0)
+    if (*needle == '\0' || len  == 0)
         return (char *)(haystack);
 
     while (*haystack != '\0')
