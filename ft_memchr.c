025311ac109fce50de23b3ecc404b5049893a311
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2019-05-25 11:29:02 +0200

    akalombo

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/25 09:33:53 by akalombo          #+#    #+#             */
+/*   Updated: 2019/05/25 11:26:51 by akalombo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void			*ft_memchr(const void *str, int c, size_t n)
+{
+	unsigned char *s;
+   	unsigned char ch;
+
+	ch = (unsigned char)(c);
+	s = (unsigned char *)(str);
+	while (n--)
+	{
+		if (*s != ch)
+			s++;
+		else
+			return(s);
+	}
+	return (0);
+}
