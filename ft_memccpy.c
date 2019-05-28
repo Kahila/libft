3d563b52dab5e0ab101ee1c7d1ad418ae9af70e8
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2019-05-28 09:40:14 +0200

    akalombo

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,26 +6,29 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 11:08:14 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/21 16:09:58 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/28 09:39:46 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void		*ft_memccpy (void *d1, const void *s1, int c, size_t size)
 {
-	char *dest = d1;
-	char const *src = s1;
+	char  *dest = (char *)d1;
+	const char *src = (const char *)s1;
 
 	while (size--)
 	{
 		*dest = *src;
-		s1++;
-		d1++;
-		printf("works\n");
-		if (*dest == c)
+		if (*src == (const char)c)
+		{
+			return ((char *)d1 + 1);
 			break;
+		}
+		dest++;
+		src++;
+		d1++;
+		s1++;
 	}
-	return (d1);
+	return (0);
 }
