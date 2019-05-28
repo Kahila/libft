6bb00504c0c07ee1640a2f59eb2ee6eb17472c90
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2019-05-28 14:36:09 +0200

    akalombo

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,10 +6,12 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 08:17:53 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/26 08:19:41 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/28 10:32:01 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h>
 #include "libft.h"
  
 void    *ft_memmove(void *dst, const void *src, size_t len)
@@ -31,4 +33,15 @@ void    *ft_memmove(void *dst, const void *src, size_t len)
             *strdst++ = *strsrc++;
  
     return (dst);
-}   
+} 
+
+int main()
+{
+	char dst[10];
+	char scr[5] = "Nathi";
+	size_t len;
+	len = 1;
+	printf("System : %s\n" , memmove(dst, scr ,len));
+	printf("ft_memmpove : %s\n", ft_memmove(dst, scr, len));
+	return (0);
+}
