3fa023829a3f607eb3f62d79d4e1ce4fffa0daa0
Adonis <adonis@localhost.localdomain>
2019-05-30 18:20:43 +0200

    akalombo

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,22 +6,28 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 11:08:11 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/18 13:37:12 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/30 18:20:11 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
+#include "libft.h"
 
 char*				ft_strncpy(char *str1, char *str2, int len)
 {
 	int i = 0;
-	while (len >= 0)
+	while (len > 0 && str2[i] != '\0')
 	{
-		str2[i] = str1[i];
+		str1[i] = str2[i];
 		i++;
 		len--;
 	}
-	str2[i] = '\0';
 	
-	return (str2);
+    while (len > 0)
+    {
+        str1[i] = '\0';
+        i++;
+        len--;
+    }
+	
+	return (str1);
 }
