96772f7c3c8f6fdf07fd75727caae2deae5f4ee4
Ado KALOMBO <akalombo@c4r5s11.wethinkcode.co.za>
2019-05-30 14:26:16 +0200

    akalombo

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 11:07:03 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/18 13:33:03 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/30 14:25:51 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 char            *ft_strcpy(char *s1, char *s2)
 {
     int intCount = 0;
-    while (s1[intCount] != '\0')
+    while (s2[intCount] != '\0')
     {
-        s2[intCount] = s1[intCount];
+        s1[intCount] = s2[intCount];
         intCount++;
     }
-     s2[intCount] = '\0';
+     s1[intCount] = '\0';
     
-    return (s2);
+    return (s1);
 }
