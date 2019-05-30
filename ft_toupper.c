50b9c39a870ea2b6034a2a1502b496419395fe51
Ado KALOMBO <akalombo@c4r5s11.wethinkcode.co.za>
2019-05-30 16:19:43 +0200

    akalombo

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 15:58:26 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/24 16:28:34 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/30 16:18:56 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 int				ft_toupper(int ch)
 {
 	char c = (char)(ch);
-	if (c >= 'A' && c <= 'Z')
-		return ((ch + 7) + 25);
+	if (c >= 'a' && c <= 'z')
+		return ((ch - 7) - 25);
 	else
-		return ((int)(c));
+		return ((c));
 }
