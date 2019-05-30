dd39f53979128a8975b989383dd4592d54941839
Ado KALOMBO <akalombo@c4r5s11.wethinkcode.co.za>
2019-05-30 16:17:35 +0200

    akalombo

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,17 +6,17 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 16:27:39 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/24 16:28:46 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/05/30 16:15:18 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int				ft_toupper(int ch)
+int				ft_tolower(int ch)
 {
 	char c = (char)(ch);
-	if (c >= 'a' && c <= 'z')
-		return ((ch - 7) - 25);
+	if (c >= 'A' && c <= 'Z')
+		return ((ch  + 7) + 25);
 	else
-		return ((int)(c));
+		return ((c));
 }
