8d642b5e447076a3155810742a43480de028fe60
Adonis <adonis@localhost.localdomain>
2019-06-01 17:20:41 +0200

    akalombo

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/01 17:00:54 by akalombo          #+#    #+#             */
+/*   Updated: 2019/06/01 17:20:15 by akalombo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void            ft_putnbr_fd(int n, int fd)
+{
+    unsigned int num;
+
+    if (n < 0)
+    {
+        num = (size_t)n * (-1);
+        ft_putchar_fd('-', fd);
+    }
+    else
+        num = (unsigned int)n;
+
+    if (num >= 10)
+        ft_putnbr_fd(num/10, fd);
+
+    ft_putchar_fd((num % 10 + '0'), fd);
+}
