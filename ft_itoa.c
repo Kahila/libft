2b4a18b1db15515df082afeec6bc87e5c8562073
Adonis <adonis@localhost.localdomain>
2019-06-04 06:24:09 +0200

    akalombo

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,64 +6,38 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 11:22:42 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/30 11:04:08 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/04 06:18:54 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char		*ft_itoa(int n)
+char	*ft_itoa(int n)
 {
+	char *str;
+	long	num;
+	int		i;
 
-	int count;
-	int i;
-	int j = 0;
-	int temp;
-	int allocated = 0;
-	char *val;
-	int zero;
-	while (n > 0)
+	num = n;
+	i = ft_int_len(num);
+	if (!(str = ft_memalloc(sizeof(char) * (i + 1))))
+		return (0);
+	str[i--] = '\0';
+	if (num == 0)
 	{
-		count = n;
-		i = 0;
-		temp = 1;
-
-		while (count > 0)
-		{
-			i++;
-			count /= 10;
-			printf("works %d\n", i);
-		}
-		if (allocated == 0)
-		{
-			printf("alocated\n");
-			allocated = 1;
-			val = (char *)malloc((i + 1) * sizeof(char));
-		}
-
-		while (i > 1)
-		{
-			temp *= 10;
-			i--;
-			//printf("temp = %d\n", temp);
-		}
-		val[j] =  n / (temp) + '0';
-		n = n - ((temp) * (n / temp));
-		//val++;
-		j++;
-		
+		str[0] = '0';
+		return (str);
 	}
-	val[j] = '\0';
-	printf("\n====%s\n", val);
-	return (val);
-}
-
-#include <stdio.h>
-
-int main()
-{
-	int inti = 16076;
-	printf("%s\n", ft_itoa(inti));
-	return (0);
+	if (num < 0)
+	{
+		str[0] = '-';
+		num = num * -1;
+	}
+	while (num > 0)
+	{
+		str[i] = '0' + (num % 10);
+		num = num / 10;
+		i--;
+	}
+	return (str);
 }
