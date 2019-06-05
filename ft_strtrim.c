3b423154ef7d814417442ac885ea431d5ff0e257
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-05 14:58:28 +0200

    akalombo

---
+++ ft_strtrim.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 08:15:33 by akalombo          #+#    #+#             */
+/*   Updated: 2019/06/05 14:55:52 by akalombo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *			ft_strtrim(char const *str)
+{
+	char *s;
+	char *str1;
+	char *rev;
+	char *new;
+	int len;
+	int count = 0;
+
+	s = (char *)str;
+	len = 0;
+
+	while((*s == '\t' || *s == '\n' || *s == ' ') && *s != '\0')
+		s++, count++;
+
+	str1 = (char *)s;
+	count = ft_strlen(str1);
+	rev = ft_memalloc(count);
+	count--;
+	while(count >= 0)
+	{
+		rev[len] = str1[count];
+		count--;
+		len++;
+	}
+	rev[len] = '\0';
+	while ((*rev == ' ' || *rev == '\t' || *rev == '\n'))
+		rev++;
+	s = rev;
+	len = ft_strlen(s);
+	new = ft_memalloc(len);
+	while (len >= 0)
+	{
+		new[count] = s[len];
+		len--;
+		count++;
+	}
+	new[count] = '\0';
+	return (new);
+}
