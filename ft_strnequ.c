c339f019cf2fcb42df95ef1908c16883dc7f99be
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-08 15:08:01 +0200

    akalombo

---
+++ ft_strnequ.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/08 13:43:57 by akalombo          #+#    #+#             */
+/*   Updated: 2019/06/08 15:07:33 by akalombo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int				ft_strnequ(char const *s1, char const *s2, size_t n)
+{
+	unsigned int len;
+	unsigned int len1;
+	unsigned int i;
+	i = 0;
+	len = ft_strlen((char *)s1);
+	len1 = ft_strlen((char *)s2);
+
+	while(n != 0 && (*s1 != '\0' || *s2 != '\0'))
+	{
+		if (*s1 == *s2)
+			i++;
+		else
+			return (0);
+		n--;
+		s1++;
+		s2++;
+	}	
+
+	return (1);
+}
