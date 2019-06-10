64599f6b535e471c2a7ecb1f13f782a252f8cda9
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 10:42:19 +0200

    akalombo normed

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,32 +6,31 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:17:08 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/23 06:04:32 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 10:42:03 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "libft.h"
-  
- char               *ft_strstr(const char *haystack, const char *needle)
- {
-    const char *s1;
-    const char *s2;
+#include "libft.h"
 
-    if (*needle == '\0')
-        return (char *)(haystack);
+char			*ft_strstr(const char *haystack, const char *needle)
+{
+	const char *s1;
+	const char *s2;
 
-     while (*haystack != '\0')
-     {
-         s1 = haystack;
-         s2 = needle;
-         while (*s2 != '\0' && *s1 == *s2)
-         {
-             s1++;
-             s2++;
-         }
-         if (*s2 == '\0')
-             return ((char *)haystack);
-         haystack++;
-     }
-     return (0);
- }
+	if (*needle == '\0')
+		return (char *)(haystack);
+	while (*haystack != '\0')
+	{
+		s1 = haystack;
+		s2 = needle;
+		while (*s2 != '\0' && *s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+			return ((char *)haystack);
+		haystack++;
+	}
+	return (0);
+}
