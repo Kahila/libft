1a4aa1d7f61a68fbf4e055cdafd302b745fac448
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 14:11:49 +0200

    akalombo normed

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,15 +6,17 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 15:48:52 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/31 15:50:52 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 14:11:34 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int			ft_isprint(int ch)
+int				ft_isprint(int ch)
 {
-	unsigned char c = (unsigned char)(ch);
+	unsigned char c;
+
+	c = (unsigned char)(ch);
 	if (c > 31 && c < 127)
 		return (1048);
 	else
