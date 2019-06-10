f7607359049d14b42ad1eb5a75c989db7c01c9ea
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 12:58:32 +0200

    akalombo normed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,26 +6,23 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 03:49:58 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/05 04:27:47 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 12:58:19 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
- 
-char *          ft_strjoin(char const *s1, char const *s2)
+
+char			*ft_strjoin(char const *s1, char const *s2)
 {
-    char *new;
-    size_t len;
- 
-    len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
-    new = ft_memalloc(len);
- 
-    while (*s1 != '\0')
-        *new++ = *s1++;
+	char	*new;
+	size_t	len;
 
-    while (*s2 != '\0')
-        *new++ = *s2++;
-
-    *new = '\0';
-    return (new - len);
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	new = ft_memalloc(len);
+	while (*s1 != '\0')
+		*new++ = *s1++;
+	while (*s2 != '\0')
+		*new++ = *s2++;
+	*new = '\0';
+	return (new - len);
 }
