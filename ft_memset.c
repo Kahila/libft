425eb50caa768a79002f090eeeec1c88eb174dc0
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 14:09:10 +0200

    akalombo normed

--- ft_memset.c
+++ ft_memset.c
@@ -6,21 +6,21 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 13:04:51 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/20 13:16:54 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 14:08:56 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void                *ft_memset(void *start, int init, size_t size)
+void			*ft_memset(void *start, int init, size_t size)
 {
-    unsigned char *ptr;
-    ptr = (unsigned char*)start;
-    while (size > 0)
-    {
-        *ptr++ = (unsigned char)init;
-        size--;
-    }
-    return (start);
+	unsigned char *ptr;
 
+	ptr = (unsigned char*)start;
+	while (size > 0)
+	{
+		*ptr++ = (unsigned char)init;
+		size--;
+	}
+	return (start);
 }
