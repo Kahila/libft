fbc171d3f288011f08da6f6fa8fe2fdb45dfb41b
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 11:32:29 +0200

    akalombo normed

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,28 +6,28 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 11:08:11 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/30 18:20:11 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 11:31:59 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char*				ft_strncpy(char *str1, char *str2, int len)
+char				*ft_strncpy(char *str1, char *str2, int len)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 	while (len > 0 && str2[i] != '\0')
 	{
 		str1[i] = str2[i];
 		i++;
 		len--;
 	}
-	
-    while (len > 0)
-    {
-        str1[i] = '\0';
-        i++;
-        len--;
-    }
-	
+	while (len > 0)
+	{
+		str1[i] = '\0';
+		i++;
+		len--;
+	}
 	return (str1);
 }
