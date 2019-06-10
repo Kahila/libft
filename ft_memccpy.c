9d6ef86d3b70c6918816b4e66ef1514e78a235a4
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 14:19:48 +0200

    akalombo normed

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,24 +6,26 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 11:08:14 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/28 09:39:46 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 14:19:34 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memccpy (void *d1, const void *s1, int c, size_t size)
+void		*ft_memccpy(void *d1, const void *s1, int c, size_t size)
 {
-	char  *dest = (char *)d1;
-	const char *src = (const char *)s1;
+	char		*dest;
+	const char	*src;
 
+	dest = (char *)d1;
+	src = (const char *)s1;
 	while (size--)
 	{
 		*dest = *src;
 		if (*src == (const char)c)
 		{
 			return ((char *)d1 + 1);
-			break;
+			break ;
 		}
 		dest++;
 		src++;
