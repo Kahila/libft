0c6f36c752e22bfffc39cc5b117e3a1a0bf59001
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 14:07:11 +0200

    akalombo normed

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,31 +6,28 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/26 08:17:53 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/04 06:30:53 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 14:06:54 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
 #include "libft.h"
- 
-void    *ft_memmove(void *dst, const void *src, size_t len)
+
+void		*ft_memmove(void *dst, const void *src, size_t len)
 {
-    char *src1;
-    char *dst1;
- 
-    src1 = (char*)src;
-    dst1 = (char*)dst;
-    if (src1 < dst1)
-    {
-        src1 = src1 + len - 1;
-        dst1 = dst1 + len - 1;
-        while (len-- > 0)
-            *dst1-- = *src1--;
-    }
-    else
-        while (len-- > 0)
-            *dst1++ = *src1++;
- 
-    return (dst);
+	char *src1;
+	char *dst1;
+
+	src1 = (char*)src;
+	dst1 = (char*)dst;
+	if (src1 < dst1)
+	{
+		src1 = src1 + len - 1;
+		dst1 = dst1 + len - 1;
+		while (len-- > 0)
+			*dst1-- = *src1--;
+	}
+	else
+		while (len-- > 0)
+			*dst1++ = *src1++;
+	return (dst);
 }
