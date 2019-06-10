9da874c2628f2fde8d393d2eb0030b3bed087add
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 13:59:34 +0200

    akalombo normed

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,20 +6,20 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 11:54:24 by akalombo          #+#    #+#             */
-/*   Updated: 2019/03/30 23:01:27 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 13:59:12 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void        ft_putstr(char *str)
+void			ft_putstr(char *str)
 {
-    int counter;
+	int counter;
 
-        counter = 0;
-        while (str[counter] != '\0')
-        {
-            ft_putchar(str[counter]);
-            counter++;
-        }
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		ft_putchar(str[counter]);
+		counter++;
+	}
 }
