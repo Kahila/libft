4832fb538dff684e82f0205ac3fcefde17ac5f01
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 13:46:37 +0200

    akalombo normed

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,18 +6,21 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 10:06:53 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/23 11:07:41 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 13:45:58 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int				ft_strcmp(const char *str1, const char *str2)
+int			ft_strcmp(const char *str1, const char *str2)
 {
+	size_t n;
+
 	while (*str1 && *str1 == *str2)
 	{
 		str1++;
 		str2++;
 	}
-	return ((unsigned  char)(*str1) - (unsigned char)(*str2));
+	n = (unsigned char)*str1 - (unsigned char)*str2;
+	return (n);
 }
