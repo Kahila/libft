f8b28fd1438b6181e797d1ae2a0f9f738dda4d6c
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 10:59:21 +0200

    akalombo normed

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,15 +6,15 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:16:59 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/23 11:38:20 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 10:59:01 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int			ft_strncmp(const char* str1, const char* str2, size_t n)
+int			ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-	while(n--)
+	while (n--)
 	{
 		str1++;
 		str2++;
