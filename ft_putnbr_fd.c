fc31cfa9c5457287257f8e05ea2d50ebece0365c
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 14:01:17 +0200

    akalombo normed

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,26 +6,24 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 17:00:54 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/01 17:20:15 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 14:00:57 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void            ft_putnbr_fd(int n, int fd)
+void			ft_putnbr_fd(int n, int fd)
 {
-    unsigned int num;
+	unsigned int num;
 
-    if (n < 0)
-    {
-        num = (size_t)n * (-1);
-        ft_putchar_fd('-', fd);
-    }
-    else
-        num = (unsigned int)n;
-
-    if (num >= 10)
-        ft_putnbr_fd(num/10, fd);
-
-    ft_putchar_fd((num % 10 + '0'), fd);
+	if (n < 0)
+	{
+		num = (size_t)n * (-1);
+		ft_putchar_fd('-', fd);
+	}
+	else
+		num = (unsigned int)n;
+	if (num >= 10)
+		ft_putnbr_fd(num / 10, fd);
+	ft_putchar_fd((num % 10 + '0'), fd);
 }
