e8584fe61480959e4efea4827a4448236deee3c0
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 13:56:14 +0200

    akalombo normed

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,19 +6,19 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 09:34:16 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/30 14:06:56 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 13:56:01 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char*			ft_strdup(char *str)
+char			*ft_strdup(char *str)
 {
-	char *strdup;
-	
-	strdup = (char *)malloc (ft_strlen(str) * sizeof(char));
-	int i;
-   	i = 0;
+	char	*strdup;
+	int		i;
+
+	strdup = ft_memalloc(ft_strlen(str));
+	i = 0;
 	while (str[i] != '\0')
 	{
 		strdup[i] = str[i];
