2fe83584055fdeba14deb9a66b825c37daee16f6
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 13:05:08 +0200

    akalombo normed

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,27 +6,28 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 11:00:16 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/20 14:31:43 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 13:04:54 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *           ft_strmap(char const *s, char (*f)(char))
+char			*ft_strmap(char const *s, char (*f)(char))
 {
-    unsigned int count = 0;
-    char* fresh;
+	unsigned int	count;
+	char			*fresh;
 
-    fresh = ft_strnew(ft_strlen((char *)s));
-    if(fresh == NULL)
-        return (NULL);
-    else
-    {
-        while (s[count] != '\0')
-        {
-            fresh[count] = f(s[count]);
-            count++;
-        }
-    }
-    return (fresh);
+	count = 0;
+	fresh = ft_strnew(ft_strlen((char *)s));
+	if (fresh == NULL)
+		return (NULL);
+	else
+	{
+		while (s[count] != '\0')
+		{
+			fresh[count] = f(s[count]);
+			count++;
+		}
+	}
+	return (fresh);
 }
