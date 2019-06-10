970c2d201df45f42ae2aecd8e2c7a59db0571ceb
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 10:54:22 +0200

    akalombo normed

--- ft_strclr.c
+++ ft_strclr.c
@@ -6,13 +6,13 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/31 10:32:11 by akalombo          #+#    #+#             */
-/*   Updated: 2019/04/16 00:05:36 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 10:53:46 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void            ft_strclr(char *s)
+void			ft_strclr(char *s)
 {
-    ft_memset (s, '\0', (sizeof(char) * ft_strlen(s) + 1));
+	ft_memset(s, '\0', (sizeof(char) * ft_strlen(s) + 1));
 }
