3ff0679933fe6d0be3bd12bcb06c771156c44fc2
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 10:49:57 +0200

    akalombo normed

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,14 +6,14 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 16:26:38 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/01 16:31:38 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 10:49:34 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void        ft_putendl_fd(char const *s, int fd)
+void		ft_putendl_fd(char const *s, int fd)
 {
-    ft_putstr_fd(s, fd);
-    write(fd, "\n", 1);
+	ft_putstr_fd(s, fd);
+	write(fd, "\n", 1);
 }
