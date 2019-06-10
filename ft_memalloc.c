8ba5ef450caa8d186c3a418c949c0418f570ee88
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 14:17:11 +0200

    akalombo normed

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,18 +6,19 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/17 13:56:10 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/30 12:32:27 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 14:16:56 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void                    *ft_memalloc(size_t size)
+void				*ft_memalloc(size_t size)
 {
-  	void *alocated;
-    alocated = (void *)malloc(size);
-    if (alocated == NULL)
-        return (NULL);
-    else
-        return (ft_memset (alocated, 0, size));
+	void *alocated;
+
+	alocated = (void *)malloc(size);
+	if (alocated == NULL)
+		return (NULL);
+	else
+		return (ft_memset(alocated, 0, size));
 }
