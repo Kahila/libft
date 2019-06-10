3a394aa485cab0df641c23790a4074b6ce1f00f2
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 08:53:04 +0200

    akalombo normed

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 09:33:53 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/28 13:26:55 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 08:52:33 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void			*ft_memchr(const void *str, int c, size_t n)
 {
 	unsigned char *s;
-   	unsigned char ch;
+	unsigned char ch;
 
 	ch = (unsigned char)(c);
 	s = (unsigned char *)(str);
@@ -24,7 +24,7 @@ void			*ft_memchr(const void *str, int c, size_t n)
 		if (*s != ch)
 			s++;
 		else
-			return(s);
+			return (s);
 	}
 	return (0);
 }
