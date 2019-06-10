5460219ff5129fb4c3c36731836b26b5a164bfcc
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-10 09:34:25 +0200

    akalombo normed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 09:26:01 by akalombo          #+#    #+#             */
-/*   Updated: 2019/05/19 11:42:26 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/10 09:34:11 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int				ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char *str1 = s1;
 	const unsigned char	*str2 = s2;
+
 	while (n--)
 	{
 		if (*str1 != *str2)
