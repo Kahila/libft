88e38ce2ca7aa134e86866508fa8a57b0c9852ed
Adonis <adonis@localhost.localdomain>
2019-06-13 06:05:58 +0200

    akalombo

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,18 +6,35 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:16:59 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/10 10:59:01 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/13 05:43:46 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int			ft_strncmp(const char *str1, const char *str2, size_t n)
+int			ft_strncmp(const char *str1, const char *str2, size_t size)
 {
-	while (n--)
+	int n;
+    int i;
+    int n2;
+
+    i = 0;
+    n = 0;
+    n2 = size;
+    while (str1[i] != '\0' && size > 0)
+    {
+        n += (unsigned char)str1[i];
+        i++;
+        size--;
+    }
+    size = n2;
+    i = 0;
+    n2 = 0;
+	while (size > 0 && str2[i] != '\0')
 	{
-		str1++;
-		str2++;
-	}
-	return ((unsigned char)(*str1) - (unsigned char)(*str2));
+        n2 += (unsigned char)str2[i];
+	    i++;
+        size--;
+    }
+	return (n - n2);
 }
