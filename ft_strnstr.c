8ec6f4115eac81895bc81ea4a285373da1328ff0
Adonis <adonis@localhost.localdomain>
2019-06-13 03:50:15 +0200

    akalombo

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,36 +6,35 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 05:50:15 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/10 12:55:00 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/13 03:49:13 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char			*ft_strnstr(const char *hay, const char *needle, size_t len)
+char			*ft_strnstr(const char *haystack, const char *needle, size_t size)
 {
 	const char *s1;
 	const char *s2;
+    size_t s;
 
 	if (*needle == '\0')
-		return (char *)(hay);
-	else if (len == 0)
-		return (NULL);
-	while (*hay != '\0')
+		return (char *)(haystack);
+	while (*haystack != '\0' && size > 0)
 	{
-		if (len != 0)
+		s1 = haystack;
+		s2 = needle;
+        s = size;
+		while (*s2 != '\0' && *s1 == *s2 && s > 0)
 		{
-			s1 = hay;
-			s2 = needle;
-			while (*s1 == *s2++ && *s2++ != '\0')
-				s1++;
-			if (*s2 == '\0')
-				return (char *)(hay);
-			hay++;
-			len--;
+			s1++;
+			s2++;
+            s--;
 		}
-		else
-			break ;
+		if (*s2 == '\0')
+			return ((char *)haystack);
+		haystack++;
+        size--;
 	}
-	return (NULL);
+	return (0);
 }
