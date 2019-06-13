397dd62314004e8f79490bd47ae7b2c062cdf30a
Adonis <adonis@localhost.localdomain>
2019-06-13 02:50:52 +0200

    akalombo

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 13:43:57 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/10 12:46:17 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/13 02:50:04 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,9 @@
 
 int				ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	unsigned int len;
-	unsigned int len1;
 	unsigned int i;
 
 	i = 0;
-	len = ft_strlen((char *)s1);
-	len1 = ft_strlen((char *)s2);
 	while (n != 0 && (*s1 != '\0' || *s2 != '\0'))
 	{
 		if (*s1 == *s2)
