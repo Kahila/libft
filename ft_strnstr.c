07303692f4213e31ccd48bc6d55ffd133d47def0
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-14 09:04:13 +0200

    akalombo normed

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,35 +6,35 @@
 /*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 05:50:15 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/13 03:49:13 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/14 09:03:54 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char			*ft_strnstr(const char *haystack, const char *needle, size_t size)
+char			*ft_strnstr(const char *hay, const char *needle, size_t size)
 {
-	const char *s1;
-	const char *s2;
-    size_t s;
+	const char	*s1;
+	const char	*s2;
+	size_t		s;
 
 	if (*needle == '\0')
-		return (char *)(haystack);
-	while (*haystack != '\0' && size > 0)
+		return (char *)(hay);
+	while (*hay != '\0' && size > 0)
 	{
-		s1 = haystack;
+		s1 = hay;
 		s2 = needle;
-        s = size;
+		s = size;
 		while (*s2 != '\0' && *s1 == *s2 && s > 0)
 		{
 			s1++;
 			s2++;
-            s--;
+			s--;
 		}
 		if (*s2 == '\0')
-			return ((char *)haystack);
-		haystack++;
-        size--;
+			return ((char *)hay);
+		hay++;
+		size--;
 	}
 	return (0);
 }
