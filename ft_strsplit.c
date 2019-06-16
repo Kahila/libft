8b4d69c3a7408ac80dc2eb3da6cce1c55d99845b
Adonis <adonis@localhost.localdomain>
2019-06-16 19:41:37 +0200

    akalombo

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,55 +6,36 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/15 10:56:29 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/15 16:49:24 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/16 19:37:02 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//#include <stdio.h>
 
 char			**ft_strsplit(char const *s, char c)
 {
-	char **str = NULL;
-	char *new;
-	int i;
-	int j;
-	int count;
+	int		i;
+    int     j;
+	char	**new;
 
-	new = (char *)s;
-	count = ft_count(new, c);
-	//printf("the value of count == %d\n", count);
-	if (count > 0)
-	{
-	str = (char **)malloc(sizeof(char*) * count);
 	i = 0;
-	while (i < count)
+    j = 0;
+	if (!s || (!(new = (char **)malloc(sizeof(char *) * (ft_count((char *)s, c) + 1)))))
+		return (NULL);
+	while (*s)
 	{
-		j = 0;
-		str[i] = (char *)malloc(sizeof(char));
-		while (*new != c)
+		while (*s == c && *s != '\0')
+			s++;
+		if (*s != c && *s != '\0')
 		{
-			str[i][j++] = *new;
-			//printf("the value of i is %d\n", i);
-			new++;
+			if (!(new[i] = ft_memalloc(ft_find_len(s, c) + 1)))
+				return (NULL);
+			while (*s != '\0' && *s != c)
+				new[i][j++] = (char)*s++;
+			new[i++][j] = '\0';
+			j = 0;
 		}
-		while (*new == c)
-			new++;
-		str[i][j] = '\0';
-//		printf("%s\n", str[i]);
-		i++;
 	}
-	}
-	return (str);
+	new[i] = NULL;
+	return (new);
 }
-
-/*int main()
-{
-//	int i = 0;
-	char *str = "*************";
-	char **new;
-	new = ft_strsplit(str, '*');
-	//fprintf(stderr, "is it here???");
-	//printf("String is = %s\n", *new);
-	return (0);
-}*/
