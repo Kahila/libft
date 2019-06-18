5e39af53f0838feb153f6cf9ed57ffbcdef8fc14
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-06-18 08:47:43 +0200

    akalombo normed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 11:55:01 by akalombo          #+#    #+#             */
-/*   Updated: 2019/06/10 10:46:59 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/06/17 10:05:59 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void		*ft_memcpy(void *s1, const void *s2, size_t size)
 	char		*dest;
 	const char	*src;
 
+	if (!s1 && !s2)
+		return (NULL);
 	dest = s1;
 	src = s2;
 	while (size--)
-	{
 		*dest++ = *src++;
-	}
 	return (s1);
 }
